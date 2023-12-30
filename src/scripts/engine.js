2bfecbd5c362dbaafc9c8942e208b5db5f2dae8f
Alan-oliveir <alandeog@poli.ufrj.br>
2023-12-30 12:51:55 -0300

    Add sound effects and fix bug in game logic

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -30,6 +30,12 @@ function countDown() {
   }
 }
 
+function playSound(audioName) {
+  let audio = new Audio(`./src/audios/${audioName}.m4a`);
+  audio.volume = 0.2;
+  audio.play();
+}
+
 function randomSquare() {
   state.view.squares.forEach((square) => {
     square.classList.remove("enemy");
@@ -37,7 +43,6 @@ function randomSquare() {
 
   let randomPosition = state.view.squares[Math.floor(Math.random() * 9)];
   randomPosition.classList.add("enemy");
-
   state.values.hitPosition = randomPosition.id;
 }
 
@@ -48,6 +53,7 @@ function addListenerHitBox() {
         state.values.result++;
         state.view.score.textContent = state.values.result;
         state.values.hitPosition = null;
+        playSound("hit");
       }
     });
   });
